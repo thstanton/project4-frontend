5eaaa3be07e6591518e6dee6ceaa57fc07506dc1
thstanton <thstanton@proton.me>
2023-11-24 13:40:53 +0000

    fixed missing key prop

--- src/components/JotterInstructions.jsx
+++ src/components/JotterInstructions.jsx
@@ -11,6 +11,7 @@ export default function JotterInstructions({ context }) {
       </p>
       {context.images && context.images.length && context.images.map(image => (
         <img
+          key={image.id}
           src={image.url}
           alt={context.title}
           className="max-w-sm mx-auto"
